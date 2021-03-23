8e329adf65f340180e985ed3155df96d507e7cad
Nuno Alves <nyansword@gmail.com>
2021-03-23 22:01:09 +0000

    add: tp4 ex4

--- TP4/ex4.cpp
+++ TP4/ex4.cpp
@@ -1,8 +1,36 @@
 #include "exercises.h"
+#include <sstream>
 
 std::string calcSum(int sequence[], unsigned long n) {
-    // TODO
-    return "";
+    const size_t size = n;
+    size_t lowest_i;
+    int sums[size+1][size]; //size(m,i)
+    uint32_t lowest_sum, sum;
+    std::stringstream ss;
+
+    for(size_t m = 0; m <= size; m++) {
+        lowest_sum = UINT32_MAX;
+        lowest_i = 0;
+
+        for(size_t i = 0; i < size - (m - 1); i++) {
+            if (m == 0) {
+                sums[m][i] = 0;
+
+                continue;
+            }
+
+            sum = sums[m][i] = sums[m-1][i] + sequence[i+m-1];
+
+            if(sum < lowest_sum) {
+                lowest_sum = sum;
+                lowest_i = i;
+            }
+        }
+
+        if(m > 0) ss << lowest_sum << "," << lowest_i << ";";
+    }
+
+    return ss.str();
 }
 
 void testPerformanceCalcSum() {
