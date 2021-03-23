275a4abb0f0e6d20d05b23bff734282094ba7ea6
Nuno Alves <nyansword@gmail.com>
2021-03-23 19:45:49 +0000

    add: tp4 ex2

--- TP4/ex2.cpp
+++ TP4/ex2.cpp
@@ -1,8 +1,62 @@
 #include "exercises.h"
+#include <iostream>
 
 bool changeMakingUnlimitedDP(unsigned int C[], unsigned int n, unsigned int T, unsigned int usedCoins[]) {
-    // TODO
-	return false;
+    const size_t coins = n + 1;
+    const size_t changes = T + 1;
+    const int err = T+1;
+    int coin_index;
+    int candidate_a, candidate_b;
+    int min_coins[coins][changes], last_coin[coins][changes];
+
+    for(int i = 0; i < n; i++) {
+        usedCoins[i] = 0;
+    }
+
+
+    for(int max_coin = 0; max_coin <= n; max_coin++) {
+        for(int change = 0; change <= T; change++) {
+            if(change == 0) {
+                min_coins[max_coin][change] = 0;
+                last_coin[max_coin][change] = 0;
+
+                continue;
+            } else if(max_coin == 0) {
+                min_coins[max_coin][change] = err;
+                last_coin[max_coin][change] = 0;
+
+                continue;
+            }
+
+            coin_index = max_coin - 1;
+
+            if(change - (int)C[coin_index] < 0) candidate_a = err;
+            else candidate_a = min_coins[max_coin][change - C[coin_index]] + 1;
+
+            candidate_b = min_coins[max_coin - 1][change];
+
+            if(candidate_a != err && ((candidate_a < candidate_b) || (candidate_b == err))) {
+                min_coins[max_coin][change] = candidate_a;
+                last_coin[max_coin][change] = coin_index;
+            } else {
+                min_coins[max_coin][change] = candidate_b;
+                last_coin[max_coin][change] = last_coin[max_coin - 1][change];
+            }
+        }
+    }
+
+    if(min_coins[n][T] == err) return false;
+
+    int change = T;
+    int coin_used;
+    while(change > 0) {
+        coin_used = last_coin[n][change];
+
+        usedCoins[coin_used]++;
+        change -= C[coin_used];
+    }
+
+	return true;
 }
 
 
