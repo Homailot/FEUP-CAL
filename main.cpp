5b32fb107bc9e8bfc524420c38e29098e159c073
Nuno Alves <nyansword@gmail.com>
2021-10-08 14:48:34 +0100

    dont know

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv) {
     srand(time(NULL));
-
+    std::cout << 9/10 + 1 << std::endl;
     std::cout << "Running all tests..." << std::endl;
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
